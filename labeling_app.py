127b3782e43a0dc584f84edea5d7823967f49b3a
Andrew Maguire <andrewm4894@gmail.com>
2023-11-21 15:46:07 +0000

    add some labels

--- labeling_app.py
+++ labeling_app.py
@@ -30,33 +30,35 @@ if "email_data" not in st.session_state:
 st.title("PII Data Labeler")
 
 # Display the current email message
-file_id = st.session_state.email_data["file"]
-file_id_clean = st.session_state.email_data["file_clean"]
-message = st.session_state.email_data["message"]
+st.session_state.file_id = st.session_state.email_data["file"]
+st.session_state.file_id_clean = st.session_state.email_data["file_clean"]
+st.session_state.message = st.session_state.email_data["message"]
 
-st.write("File ID:", file_id)
-st.text_area("Email Message", message, height=500)
+st.write("File ID:", st.session_state.file_id)
+st.text_area("Email Message", st.session_state.message, height=500)
 
 # Input boxes for PII in columns
 st.subheader("Highlight and Enter PII Information")
 
 col1, col2, col3, col4 = st.columns(4)
-name = col1.text_area("Names", height=100)
-phone_number = col2.text_area("Phone Numbers", height=100)
-email_address = col3.text_area("Email Addresses", height=100)
-physical_address = col4.text_area("Physical Addresses", height=100)
+st.session_state.name = col1.text_area("Names", value="", height=100)
+st.session_state.phone_number = col2.text_area("Phone Numbers", value="", height=100)
+st.session_state.email_address = col3.text_area("Email Addresses", value="", height=100)
+st.session_state.physical_address = col4.text_area("Physical Addresses", value="", height=100)
 
 # Save Button
 if st.button("Save PII Data"):
     pii_data = {
-        "file_id": file_id,
-        "names": name.split("\n"),
-        "phone_numbers": phone_number.split("\n"),
-        "email_addresses": email_address.split("\n"),
-        "physical_addresses": physical_address.split("\n"),
+        "file_id": st.session_state.file_id,
+        "names": list(set(st.session_state.name.split("\n"))),
+        "phone_numbers": list(set(st.session_state.phone_number.split("\n"))),
+        "email_addresses": list(set(st.session_state.email_address.split("\n"))),
+        "physical_addresses": list(set(st.session_state.physical_address.split("\n"))),
     }
+    for key in ["email_addresses"]:
+        pii_data[key] = [x.lower() for x in pii_data[key]]
 
-    output_path = f"./data/labels/{file_id_clean}.json"
+    output_path = f"./data/labels/{st.session_state.file_id_clean}.json"
     with open(output_path, "w") as f:
         json.dump(pii_data, f)
 
@@ -65,8 +67,4 @@ if st.button("Save PII Data"):
 # Button to load a new random email
 if st.button("Sample Another Email"):
     st.session_state.email_data = load_random_row()
-    st.rerun()
-
-# Button to clear inputs
-if st.button("Clear Inputs"):
     st.rerun()
