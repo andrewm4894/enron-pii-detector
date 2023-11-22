179afca81372d998b0017cead6b91757b317c819
Andrew Maguire <andrewm4894@gmail.com>
2023-11-22 17:55:56 +0000

    Refactor get_tools function to include required parameters in a list

--- src/openai.py
+++ src/openai.py
@@ -11,7 +11,12 @@ def get_tools():
                 "name": "extract_pii_entities",
                 "description": "Extract any PII related entities (`names`, `phone_numbers`, `email_addresses`, `physical_addresses`) found in the message(s). If there are none found, return an empty list for that type.",
                 "parameters": MessageEntities.model_json_schema(),
-                "required": ["names", "phone_numbers", "email_addresses", "physical_addresses"],
+                "required": [
+                    "names",
+                    "phone_numbers",
+                    "email_addresses",
+                    "physical_addresses",
+                ],
             },
         }
     ]
